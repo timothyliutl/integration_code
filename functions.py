2cc34a187c560db2fd8591ecffed4d51e0d27f59
timothyliutl <18tfl1@queensu.ca>
2024-08-27 15:40:59 -0400

    updated code

--- functions.py
+++ functions.py
@@ -45,7 +45,7 @@ def convolution2(x_val, t_val, convolution_array, step_size):
     for x_index in range(len(x_val)):
         sum_val = 0
         for t in t_val:
-            sum_val += convolution_array[int((x_val[x_index]-t+10)/0.01)]*dist_w2(t)*step_size
+            sum_val += convolution_array[int((x_val[x_index]-t+40)/0.005)]*dist_w2(t)*step_size
         return_array[x_index] = sum_val
     return return_array
 
@@ -67,19 +67,22 @@ def f_x_given_z(x,z, conv_val_y1w1, conv_val_yw1w2):
     return exp_dist(z-x)*conv_val_y1w1/conv_val_yw1w2
 
 @cuda.jit
-def inner_int_matrix(x_array, y_array, z_array, alpha, convolution_array_w1w2, convolution_array_w1w2y, convolution_array_w1y, return_matrix):
+def inner_int_matrix(x_array, y_array, z_array, step_size, lower_bound, alpha, convolution_array_w1w2, convolution_array_w1w2y, convolution_array_w1y, return_matrix):
     x_pos, y_pos, z_pos = cuda.grid(3)
     if x_pos < len(x_array) and y_pos < len(y_array) and z_pos < len(z_array):
         #funct1_val =  math.log10(f_x_given_y_z(x_array[x_pos], y_array[y_pos], z_array[z_pos], convolution_array_w1w2[int((y_array[y_pos] - z_array[z_pos]+10)/0.01)])/(f_x_given_z(x_array[x_pos], z_array[z_pos], convolution_array_w1y[int((x_array[x_pos]+10)/0.01)], convolution_array_w1w2y[int((z_array[z_pos]+10)/0.01)])))
         #funct2_val = f_x_given_y_z(x_array[x_pos], y_array[y_pos], z_array[z_pos], convolution_array_w1w2[int((y_array[y_pos] - z_array[z_pos]+10)/0.01)])*convolution_array_w1w2[int((z_array[z_pos]- y_array[y_pos]+10)/0.01)] * dist_y(y_array[y_pos])
-        
-        funct1_val = f_x_given_y_z(x_array[x_pos], y_array[y_pos], z_array[z_pos], convolution_array_w1w2[int((y_array[y_pos] - z_array[z_pos]+10)/0.01)])**alpha
-        funct2_val = f_x_given_z(x_array[x_pos], z_array[z_pos], convolution_array_w1y[int((x_array[x_pos]+10)/0.01)], convolution_array_w1w2y[int((z_array[z_pos]+10)/0.01)])**(1-alpha)
-        return_matrix[x_pos, y_pos, z_pos] =  funct1_val*funct2_val 
+        f_y_z_val = convolution_array_w1w2[int((z_array[z_pos]- y_array[y_pos]-lower_bound)/step_size)] * dist_y(y_array[y_pos])
+
+        funct1_val = f_x_given_y_z(x_array[x_pos], y_array[y_pos], z_array[z_pos], convolution_array_w1w2[int((y_array[y_pos] - z_array[z_pos]-lower_bound)/step_size)])**alpha
+        funct2_val = f_x_given_z(x_array[x_pos], z_array[z_pos], convolution_array_w1y[int((x_array[x_pos]- lower_bound)/step_size)], convolution_array_w1w2y[int((z_array[z_pos]-lower_bound)/step_size)])**(1-alpha)
+        return_matrix[x_pos, y_pos, z_pos] = funct1_val*funct2_val
+        #return_matrix[x_pos, y_pos, z_pos] = math.log(funct1_val/funct2_val)* funct1_val 
 
 @cuda.jit
-def inner_int_division_matrix(y_array, z_array, inner_int_matrix, convolution_array_w1w2, return_matrix):
+def inner_int_division_matrix(y_array, z_array, step_size, lower_bound, inner_int_matrix, convolution_array_w1w2, return_matrix):
     y_pos, z_pos = cuda.grid(2)
     if y_pos< len(y_array) and z_pos < len(z_array):
-        f_y_z_val = convolution_array_w1w2[int((z_array[z_pos]- y_array[y_pos]+10)/0.01)] * dist_y(y_array[y_pos])
-        return_matrix[y_pos, z_pos] = inner_int_matrix[y_pos, z_pos] * f_y_z_val
+        f_y_z_val = convolution_array_w1w2[int((z_array[z_pos]- y_array[y_pos]-lower_bound)/step_size)] * dist_y(y_array[y_pos])
+        #return_matrix[y_pos, z_pos] = f_y_z_val
+        return_matrix[y_pos, z_pos] = math.log(inner_int_matrix[y_pos, z_pos]) * f_y_z_val * (0.005)**2
--- script.py
+++ script.py
@@ -3,44 +3,81 @@ from functions import inner_int_matrix, convolution, convolution2, convolution_w
 import math
 import pandas as pd
 
-x_val = np.arange(-5,5,0.01)
-y_val = np.arange(-5,5,0.01)
-z_val = np.arange(-5,5,0.01)
+# x_val = np.arange(-12,12,0.02)
+# y_val = np.arange(-8,8,0.02)
+# z_val = np.arange(-8,8,0.02)
 
 
-threadsperblock = (10, 10, 10)
-blockspergrid_x = math.ceil(x_val.shape[0] / threadsperblock[0])
-blockspergrid_y = math.ceil(x_val.shape[0] / threadsperblock[1])
-blockspergrid_z = math.ceil(x_val.shape[0] / threadsperblock[2])
-blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
+# threadsperblock = (10, 10, 10)
+# blockspergrid_x = math.ceil(x_val.shape[0] / threadsperblock[0])
+# blockspergrid_y = math.ceil(x_val.shape[0] / threadsperblock[1])
+# blockspergrid_z = math.ceil(x_val.shape[0] / threadsperblock[2])
+# blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
 
-
-convolution_x_val = np.arange(-10,10,0.01)
-t_val = np.arange(-5,5,0.01)
-convolution_array_yw1 = convolution(convolution_x_val, t_val, 0.01)
-convolution_array_yw1w2 = convolution2(convolution_x_val, t_val, convolution_array_yw1, 0.01)
-#np.savetxt("foo.csv", convolution_array2, delimiter=",")
-convolution_array_w1w2 = convolution_w1_w2(convolution_x_val, t_val, 0.01)
 
 df = pd.DataFrame(columns=['alpha', 'value'])
 
-for alpha in np.arange(0.001,0.999, 0.01):
-    result_matrix = np.zeros((x_val.shape[0],x_val.shape[0],x_val.shape[0]))
-    print('calculating for alpha = ', alpha)
-    inner_int_matrix[blockspergrid, threadsperblock](x_val, y_val, z_val,alpha, convolution_array_w1w2, convolution_array_yw1w2, convolution_array_yw1, result_matrix)
-    result_matrix = np.sum(result_matrix, axis=0)* 0.01
-    def myfunc(x):
-        return math.log10(x)
-    vfunc = np.vectorize(myfunc)
-    result_matrix = vfunc(result_matrix)
-    print(result_matrix[0:5,0:5])
+# threadsperblock_2d = (32,32)
+# blockspergrid_x = math.ceil(x_val.shape[0] / threadsperblock[0])
+# blockspergrid_y = math.ceil(x_val.shape[0] / threadsperblock[1])
+# blockspergrid_2d = (blockspergrid_x, blockspergrid_y)
 
-    final_result = np.zeros(result_matrix.shape)
-    inner_int_division_matrix[blockspergrid, threadsperblock](y_val, z_val, result_matrix, convolution_array_w1w2, final_result)
-    final_result = final_result * ((0.01)**2)
-    print(final_result[0:5,0:5])
-    print(np.sum(final_result)/(alpha-1))
-    new_row = pd.DataFrame({'alpha': [alpha], 'value': [np.sum(final_result)/(alpha-1)]})
+convolution_x_val = np.arange(-40,40,0.005)
+t_val = np.arange(-40,40,0.005)
+convolution_array_yw1 = convolution(convolution_x_val, t_val, 0.005)
+
+convolution_x_val = np.arange(-20,20,0.005)
+t_val = np.arange(-20,20,0.005)
+convolution_array_yw1w2 = convolution2(convolution_x_val, t_val, convolution_array_yw1, 0.005)
+convolution_array_yw1 = convolution(convolution_x_val, t_val, 0.005)
+convolution_array_w1w2 = convolution_w1_w2(convolution_x_val, t_val, 0.005)
+
+
+
+for alpha in np.arange(0.8,1,0.01):
+    sum_val = 0
+    for i in range(8):
+        for j in range(8):
+
+            x_val = np.arange(-12,12,0.005)
+            y_val = np.arange(2*i-8,2*i-6,0.005)
+            z_val = np.arange(2*j-8,2*j-6,0.005)
+
+            threadsperblock = (10, 10, 10)
+            blockspergrid_x = math.ceil(x_val.shape[0] / threadsperblock[0])
+            blockspergrid_y = math.ceil(x_val.shape[0] / threadsperblock[1])
+            blockspergrid_z = math.ceil(x_val.shape[0] / threadsperblock[2])
+            blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
+
+            threadsperblock_2d = (32,32)
+            blockspergrid_x = math.ceil(x_val.shape[0] / threadsperblock[0])
+            blockspergrid_y = math.ceil(x_val.shape[0] / threadsperblock[1])
+            blockspergrid_2d = (blockspergrid_x, blockspergrid_y)
+
+
+        #for alpha in [0.99]:
+            result_matrix = np.zeros((x_val.shape[0],y_val.shape[0],z_val.shape[0]))
+            print('calculating for alpha, j, i = ', alpha, j, i)
+            inner_int_matrix[blockspergrid, threadsperblock](x_val, y_val, z_val, 0.005, convolution_x_val[0] ,alpha, convolution_array_w1w2, convolution_array_yw1w2, convolution_array_yw1, result_matrix)
+            #np.savetxt("fx_yz.csv", result_matrix[:,500,500], delimiter=",")
+            result_matrix = np.sum((result_matrix*(0.005)), axis=1)
+            print(result_matrix[300:350, 300:350])
+
+            
+            # new_row = pd.DataFrame({'alpha': [alpha], 'value': [(result_matrix[0,0])]})
+            # df = pd.concat([df, new_row])
+            # df.to_csv('data-int4.csv')
+
+            final_result = np.zeros(result_matrix.shape)
+            inner_int_division_matrix[blockspergrid_2d, threadsperblock_2d](y_val, z_val, 0.005, convolution_x_val[0], result_matrix, convolution_array_w1w2, final_result)
+            #np.savetxt("pdf.csv", np.sum(final_result* ((0.02)), axis=1), delimiter=",")
+            #final_result = final_result * ((0.02)**2)
+            #value somehow negative
+
+
+
+            print(np.sum(final_result)/(alpha-1))
+            sum_val = sum_val + np.sum(final_result)
+    new_row = pd.DataFrame({'alpha': [alpha], 'value': [sum_val/(alpha-1)]})
     df = pd.concat([df, new_row])
-
-    df.to_csv('data.csv')
+    df.to_csv('data_modified_5.csv')
